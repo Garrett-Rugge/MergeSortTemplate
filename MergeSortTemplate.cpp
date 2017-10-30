3df1126669aa4e0675c76874e201710eeb8d7bdb
Garrett-Rugge <32379208+Garrett-Rugge@users.noreply.github.com>
2017-10-30 16:23:09 -0700

    Update MergeSortTemplate.cpp

--- MergeSortTemplate.cpp
+++ MergeSortTemplate.cpp
@@ -1,39 +1,24 @@
 #include <iostream>
 #include <cassert>
-#include <vector>
 
 using namespace std;
 
-template<typename T>
-class User{
-	private:
-		int len;
-		T data;
-	public:	
-	User() { len = 0; }
-	~User();
-	bool sorted(T a[], int n);
-	void merge(T a[], T x[], int start, int mid, int end);
-	void msort(T a[], T x[], int start, int end);
-	void mergesort(T a[], int len);
-};
 template<class T>
-bool User<T>::sorted(T a[], int n) {//checks if array is sorted
+bool sorted(T a[], int n) {//checks if array is sorted
 
     if (n == 1 || n == 0)
         return true;
  	int j = 1;
  	for(int i = 0; i < n; ++i){
 		if(a[i] > a[i+1])
-			j = 0;
-	 }	
-	 if(j = 0)
-	 	return false;
-	else
-    	return true;
+			return false;
+		else
+			return true;
+	}
 }
+
 template<class T>
-void User<T>::merge(T a[], T x[], int start, int mid, int end){
+void merge(int a[], int x[], T start, T mid, T end){ //merges arrays together in order
     T num1 = mid - start + 1; //lower part of the array start point
     T num2 =  end - mid; //Higher part of the array start point
  	int k, i, j;
@@ -48,7 +33,7 @@ void User<T>::merge(T a[], T x[], int start, int mid, int end){
     i = 0; 
     j = 0; 
     k = start;
-    while (i < num1 && j < num2)
+    while (i < num1 && j < num2) //sorts most if not all numbers
     {
         if (arr1[i] <= arr2[j])
         {
@@ -63,43 +48,58 @@ void User<T>::merge(T a[], T x[], int start, int mid, int end){
         k++;
     }
  
-    while (i < num1)
+    while (i < num1) //sorts any remaining in the lower array
     {
         a[k] = arr1[i];
         i++;
         k++;
     }
  
-    while (j < num2)
+    while (j < num2) //sorts any remaining in the upper array
     {
         a[k] = arr2[j];
         j++;
         k++;
     }	
 }
+
 template<class T>
-void User<T>::msort(T a[], T x[], int start, int end){
+void msort(int a[], int x[], T start, T end){ //recursive function to seperate the array
+	T mid = (start + end) / 2;
 	if(start < end){
-	int mid = (start + end) / 2;
-	msort(a, x, start, mid);
-	msort(a, x, mid + 1, end);
-	merge(a, x, start, mid, end);
-}
+		msort(a, x, start, mid);
+		msort(a, x, mid + 1, end);
+		merge(a, x, start, mid, end);
+	}
 	return;
 }
+
 template<class T>
-void User<T>::mergesort(T a[], int len){
-	data = new int[len];
-	msort(a, data, 0, len-1);
+void mergesort(T a[], int len){
+	T x[len];
+	msort(a, x, 0, len-1);
 }
 
-
-int main()
+int main() //tests for the mergesort
 {
-	User<int> a;
+	int a[1000];
+	for (int i = 0; i < 1000; ++i) a[i] = -50 + rand() % 100;
+	mergesort(a, 1000);
+	assert(sorted(a, 1000));
 	
+	int b[1001];
+	for (int i = 0; i < 1001; ++i) b[i] = -50 + rand() % 100;
+	mergesort(b, 1001);
+	assert(sorted(b, 1001));
+
+	int c[] = { 2 };
+	mergesort(c, 1);
+	assert(sorted(c, 1));
+
+	int d[] = { 1, 2, 3, 4, 5 };
+	mergesort(d, 5);
+	assert(sorted(d, 5));
+
 	
 	cout << "All tests passed." << endl;	
-	return 0;
 }
-
