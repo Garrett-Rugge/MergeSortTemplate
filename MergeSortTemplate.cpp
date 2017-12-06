fa795fc572da9fde33ccf90313ee5a1ef5c932d2
Garrett Rugge <Garrett.Rugge@gmail.com>
2017-12-06 12:28:34 -0800

    x

--- MergeSortTemplate.cpp
+++ MergeSortTemplate.cpp
@@ -4,26 +4,43 @@
 
 using namespace std;
 
-template<typename T>
+template<class T>
 class User{
 private:
 	int len;
-	T data;
+	T * data;
 public:	
-	User() { len = 0; }
+	User(int len);
 	~User();
+	T & operator[](int i);
 	bool sorted(T a[], int n);
-	void merge(T a[], T x[], int start, int mid, int end);
-	void msort(T a[], T x[], int start, int end);
-	void mergesort(T a[], int len);};
+	void merge(T x[], int start, int mid, int end);
+	void msort(T x[], int start, int end);
+	void mergesort(int len);
+};
+
 template<class T>
-bool User<T>::sorted(T a[], int n) {//checks if array is sorted
+User<T>::User(int len){
+	len = len;
+	data = new T[len];
+}
+template<class T>
+User<T>::~User(){
+	delete []data;
+	data = NULL;
+}
+template<class T>
+T & User<T>::operator[](int i){
+	return data[i];
+}
+template<class T>
+bool User<T>::sorted(int n) {//checks if array is sorted
 
     if (n == 1 || n == 0)
         return true;
  	int j = 1;
  	for(int i = 0; i < n; ++i){
-		if(a[i] > a[i+1])
+		if(data[i] > data[i+1])
 			j = 0;
 	 }	
 	 if(j = 0)
@@ -32,16 +49,16 @@ bool User<T>::sorted(T a[], int n) {//checks if array is sorted
     	return true;
 }
 template<class T>
-void User<T>::merge(T a[], T x[], int start, int mid, int end){
-    T num1 = mid - start + 1; //lower part of the array start point
-    T num2 =  end - mid; //Higher part of the array start point
+void User<T>::merge(T x[], int start, int mid, int end){
+    int num1 = mid - start + 1; //lower part of the array start point
+    int num2 =  end - mid; //Higher part of the array start point
  	int k, i, j;
     T arr1[num1], arr2[num2]; //2 temporary arrays
  
     for (i = 0; i < num1; i++)
-        arr1[i] = a[start + i];
+        arr1[i] = data[start + i];
     for (j = 0; j < num2; j++)
-        arr2[j] = a[mid + 1+ j];
+        arr2[j] = data[mid + 1+ j];
  
   
     i = 0; 
@@ -51,12 +68,12 @@ void User<T>::merge(T a[], T x[], int start, int mid, int end){
     {
         if (arr1[i] <= arr2[j])
         {
-            a[k] = arr1[i];
+            data[k] = arr1[i];
             i++;
         }
         else
         {
-            a[k] = arr2[j];
+            data[k] = arr2[j];
             j++;
         }
         k++;
@@ -64,40 +81,48 @@ void User<T>::merge(T a[], T x[], int start, int mid, int end){
  
     while (i < num1)
     {
-        a[k] = arr1[i];
+        data[k] = arr1[i];
         i++;
         k++;
     }
  
     while (j < num2)
     {
-        a[k] = arr2[j];
+        data[k] = arr2[j];
         j++;
         k++;
     }	
 }
+
 template<class T>
-void User<T>::msort(T a[], T x[], int start, int end){
+void User<T>::msort(T x[], int start, int end){
 	if(start < end){
 	int mid = (start + end) / 2;
-	msort(a, x, start, mid);
-	msort(a, x, mid + 1, end);
-	merge(a, x, start, mid, end);
+	msort(x, start, mid);
+	msort(x, mid + 1, end);
+	merge(x, start, mid, end);
+	}	
 }
 
+template<class T>
+void User<T>::mergesort(int len){
+	data = new T[len];
+	msort(data, 0, len-1);
 }
 template<class T>
-void User<T>::mergesort(T a[], int len){
-	data = new int[len];
-	msort(a, data, 0, len-1);
+void test1(){
+	User<int> a(5);
+	a[0] = 2;
+	a[1] = 1;
+	a[2] = 3;
+	a[3] = 7;
+	a[4] = 11;
+	a.mergesort(5);
+	assert(a.sorted(5));
 }
-
-
 int main()
 {
-	User<int> a;
-	
-	
+	test1();
 	cout << "All tests passed." << endl;	
 	return 0;
 }
